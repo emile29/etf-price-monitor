54b5cc106726b841312f1d7b807f0cc37d2c2ecc
Emile Li Tim Cheong <emile.ltc@gmail.com>
2026-04-19 13:38:36 -0400

    add etf data validation

--- backend/models/ETF.js
+++ backend/models/ETF.js
@@ -1,5 +1,6 @@
 class ETF {
   constructor(data) {
+    if (!this.isValidData(data)) throw new Error("Invalid CSV data!");
     this.data = data;
     this.weightMap = new Map();
     const constituentChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -7,6 +8,17 @@ class ETF {
     for (let c of constituentArr) {
       this.weightMap.set(c, (this.data.find(e => e.name === c))?.weight || 0);
     }
+  }
+
+  isValidData(data) {
+    // Assuming that a correctly formatted ETF file will only have the 'name' and 'weight' fields
+    const requiredKeys = ['name', 'weight'];
+    for (let item of data) {
+      if (!requiredKeys.every(key => key in item)) {
+        return false;
+      }
+    }
+    return true;
   }
 
   getWeight(c) {
@@ -17,4 +29,5 @@ class ETF {
     return this.data;
   }
 }
+
 module.exports = ETF;
